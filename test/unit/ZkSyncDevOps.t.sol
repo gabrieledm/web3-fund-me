3278817ec74278c2922981e240abcd6445934890
Gabriele Del Monte <gabriele.delmonte@proton.me>
2025-08-24 21:57:50 +0200

    test: Fix for zkSync failing test

--- test/unit/ZkSyncDevOps.t.sol
+++ test/unit/ZkSyncDevOps.t.sol
@@ -25,7 +25,7 @@ contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
     // Remove the `onlyVanillaFoundry`, then run `foundryup-zksync` and then
     // `forge test --mt testZkSyncFoundryFails --zksync`
     // and this will fail!
-    function testZkSyncFoundryFails() public onlyVanillaFoundry {
+    function testZkSyncFoundryFails() public skipZkSync {
         bool exists = vm.keyExistsJson('{"hi": "true"}', ".hi");
         assert(exists);
     }
